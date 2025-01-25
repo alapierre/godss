0d1c04a6683d2b81d72ded3563f5f2feabb36ea6
Adrian Lapierre <al@itrust.dev>
2025-01-25 15:39:34 +0100

    Add signature verification in pkcs11_test.go
    Enhanced the PKCS#11 test to include verification of RSA signatures using a public key extracted from the certificate. Improved test clarity by introducing additional variable assignments and updating error messages for better context. Skipped the test when the PIN environment variable is not set.

--- signer/pkcs11_test.go
+++ signer/pkcs11_test.go
@@ -1,18 +1,24 @@
 package signer
 
 import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
 	"encoding/base64"
+	"fmt"
 	"os"
 	"testing"
 )
 
 func TestSign(t *testing.T) {
 
-	mod := "/opt/proCertumSmartSign/libcryptoCertum3PKCS.so"
 	pin := os.Getenv("PIN")
 	if pin == "" {
-		t.Errorf("PIN envirnoment variable is not set")
+		t.Skip("Pomijanie testu: brak PIN do karty inteligentnej (zmienna środowiskowa PIN nie jest ustawiona)")
 	}
+
+	mod := "/opt/proCertumSmartSign/libcryptoCertum3PKCS.so"
 
 	sig, err := NewPkcs11Signer(Pkcs11Config{
 		Pkcs11ModulePath: mod,
@@ -26,7 +32,8 @@ func TestSign(t *testing.T) {
 		t.Error(err)
 	}
 
-	signed, err := sig.SignString("Hello World!")
+	originalText := "Hello World!"
+	signed, err := sig.SignString(originalText)
 
 	if err != nil {
 		t.Error(err)
@@ -34,4 +41,23 @@ func TestSign(t *testing.T) {
 
 	t.Logf("Signed: %s", base64.StdEncoding.EncodeToString(signed))
 
+	b, _ := sig.GetCerts()
+	cert, err := x509.ParseCertificate(b[0])
+	if err != nil {
+		t.Errorf("Błąd parsowania certyfikatu: %v\n", err)
+	}
+
+	publicKey, ok := cert.PublicKey.(*rsa.PublicKey)
+	if !ok {
+		t.Errorf("Klucz publiczny nie jest typu RSA")
+	}
+
+	hashed := sha256.Sum256([]byte(originalText))
+
+	err = rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hashed[:], signed)
+	if err != nil {
+		t.Errorf("Podpis nie jest prawidłowy: %v\n", err)
+	} else {
+		fmt.Println("Podpis jest prawidłowy")
+	}
 }
